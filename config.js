1c76109d609ff082b40bf733004ae68b727ba77a
danhthin <85323702+danhthin@users.noreply.github.com>
2021-06-04 09:04:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    introTitle: 'Cậu à!',
-    introDesc: `Trai Hà Tây như cây thuốc phiện0
+    introTitle: 'Quỳnh à!',
+    introDesc: `Trai Hà Tây như cây thuốc phiện
     Gái ba miền không nghiện thì cũng phê`,
     btnIntro: 'hihi',
-    title: 'ít tuổi hơn thì lễ phép mà gọi anh đi !! ',
+    title: 'ít tuổi hơn thì lễ phép mà gọi anh đi( 99 nhỏ hơn 2k là rõ ràng rồi) !! ',
     desc: 'Nhơn nhơn là không có được ',
     btnYes: 'Đồng ý gọi là anh',
     btnNo: 'Không đời nào!',
     question:'Ngoan vậy là tốt.',
     btnReply: 'Gửi cho anh Thìn đẹp troai',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Là em sẽ bấm vào mà',
-    messDesc: 'Gửi lại cho anh 1 tấm ảnh cute đi',
+    reply: 'Hí hí hí',
+    mess: 'Tớ biết mà',
+    messDesc: 'Gửi lại cho tớ 1 tấm ảnh cute pho mai we đi',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
