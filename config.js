8c9e27a4979927e2f3310673ac563cfc3125349e
danhthin <85323702+danhthin@users.noreply.github.com>
2021-06-04 09:00:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Quỳnh à!',
+    introDesc: `Trai Hà Tây như cây thuốc phiện 
+    gái ba miền không nghiện cũng phê`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    title: 'ít tuổi hơn thì lễ phép mà gọi anh đi',
+    desc: 'Nhơn nhơn là không có được ',
+    btnYes: 'Đồng ý gọi',
+    btnNo: 'Không đời nào',
+    question:'Ngoan vậy là tốt',
+    btnReply: 'Gửi cho anh Thìn đẹp troai',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Tớ biết mà 😘😘',
+    messDesc: 'Gửi cho tớ 1 tấm ảnh cute phô mai we đi',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
